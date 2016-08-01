743daf6f66205015ebfc92e8071ad1f95ad08603
guodong <413586274@qq.com>
2016-08-01 15:21:24 +0800

    Initial Commit

---
+++ SHA1Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHA1Test
+//
+//  Created by guodong on 16/8/1.
+//  Copyright © 2016年 nonobank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHA1Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHA1Test
+//
+//  Created by guodong on 16/8/1.
+//  Copyright © 2016年 nonobank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
